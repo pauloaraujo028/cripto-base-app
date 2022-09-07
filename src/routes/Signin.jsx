5a75cc47290e7fb908f6fa518914be1eca40b08d
Paulo Araújo <105018299+pauloaraujo028@users.noreply.github.com>
2022-09-07 03:41:56 -0300

    Ajuste na pagina de login e cadastro

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -25,7 +25,7 @@ const Signin = () => {
   return (
     <div>
       <div className="max-w-[400px] mx-auto min-h-[600px] px-4 py-20">
-        <h1 className="text-2xl font-bold">Sign In</h1>
+        <h1 className="text-2xl font-bold">Login</h1>
         <form onSubmit={handleSubmit}> 
           <div className="my-4">
             <label>Email</label>
@@ -36,16 +36,16 @@ const Signin = () => {
             </div>
           </div>
           <div className="my-4">
-            <label>Password</label>
+            <label>Senha</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
               <input onChange={(e) => setPassword(e.target.value)} 
               className="w-full p-2 bg-primary border border-input rounded-2xl" type="password" />
               <AiFillLock className="absolute right-2 top-3 text-gray-400" />
             </div>
           </div>
-          <button className="w-full my-2 p-3 bg-button text-btnText rounded-xl shadow-xl">Sign in</button>
+          <button className="w-full my-2 p-3 bg-button text-btnText rounded-xl shadow-xl">Entrar</button>
         </form>
-        <p className="my-4">Don't have an account? <Link to="/signup" className="text-accent">Sign up</Link></p>
+        <p className="my-4 flex justify-center">Não tem uma conta? <Link to="/signup" className="text-accent mx-1 uppercase">Inscreva-se</Link></p>
       </div>
     </div>
   ) 
--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
   return (
     <div>
       <div className="max-w-[400px] mx-auto min-h-[600px] px-4 py-20">
-        <h1 className="text-2xl font-bold">Sign Up</h1>
+        <h1 className="text-2xl font-bold">Criar Conta</h1>
         {error ? <p className="bg-red-300 p-3 my-2">{error}</p> : null}
         <form onSubmit={handleSubmit}> 
           <div className="my-4">
@@ -37,16 +37,16 @@ const Signup = () => {
             </div>
           </div>
           <div className="my-4">
-            <label>Password</label>
+            <label>Senha</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
               <input onChange={(e) => setPassword(e.target.value)}
               className="w-full p-2 bg-primary border border-input rounded-2xl" type="password" />
               <AiFillLock className="absolute right-2 top-3 text-gray-400" />
             </div>
           </div>
-          <button className="w-full my-2 p-3 bg-button text-btnText rounded-xl shadow-xl">Sign up</button>
+          <button className="w-full my-2 p-3 bg-button text-btnText rounded-xl shadow-xl">Inscrever-se</button>
         </form>
-        <p className="my-4">Already have an account? <Link to="/signin" className="text-accent">Sign in</Link></p>
+        <p className="my-4 flex justify-center">Já tem uma conta? <Link to="/signin" className="text-accent mx-1 uppercase">Entrar</Link></p>
       </div>
     </div>
   )
